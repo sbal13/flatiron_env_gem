434ab8c5e2058c5479b7617d781fceada6016954
sbal13 <sbal13@gmail.com>
2018-11-30 10:18:07 -0500

    change bash commands to ruby

--- lib/flatiron_env.rb
+++ lib/flatiron_env.rb
@@ -5,8 +5,44 @@ module FlatironEnv
 
 	class Validator
 		def self.get_and_run_script
-			exec('curl -so- https://raw.githubusercontent.com/hysan/flatiron-manual-setup-validator/master/manual-setup-check.sh | bash 2> /dev/null
-')
+			
+		end
+
+		def self.evaluate_test(eval_string)
+			result = `${eval_string}`
+
+			return result ? "pass" : "fail"
+		end
+
+		def self.evaluate(eval_string)
+			result = `${eval_string}`
+
+			return result ? "pass" : "fail"
+		end
+	end
+
+	class CLI
+
+		def self.print_table_results(name, eval_string)
+			result = FlatironEnv::Validator.evaluate_test(eval_string)
+			puts "${name} => [ ${result} ]"
+		end
+
+		def self.print_data_row
+			result = FlatironEnv::Validator.evaluate(eval_string)
+			puts "${name} => [ ${result} ]"
+		end
+
+		def self.delimiter
+			puts "******************************************"
+		end
+
+		def self.validation_header
+			puts "************ VALIDATING SETUP ************"
+		end
+
+		def self.configuration_header
+			puts "************* CONFIGURATION **************"
 		end
 	end
 end
